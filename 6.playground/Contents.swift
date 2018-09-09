cf6ef6fd8c0099f0207ae4a9538babd57941349d
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-09-09 17:58:49 +0300

    add day 6 part two

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -14,31 +14,65 @@ func readInput() -> String {
 
 let input = readInput()
 
-var blocks = input.split(separator: "\t").map { Int($0)! }
-let initialState = blocks
-var states = [initialState]
-
-var counter = 0
-while true {
-	var max = Int.min
-	var index = 0
-	for i in 0..<blocks.count {
-		if blocks[i] > max {
-			max = blocks[i]
-			index = i
+func partOne() -> Int {
+	var blocks = input.split(separator: "\t").map { Int($0)! }
+	let initialState = blocks
+	var states = [initialState]
+	
+	var counter = 0
+	while true {
+		var max = Int.min
+		var index = 0
+		for i in 0..<blocks.count {
+			if blocks[i] > max {
+				max = blocks[i]
+				index = i
+			}
 		}
+		blocks[index] = 0
+		var iteration = index < blocks.count - 1 ? index + 1 : 0
+		while max > 0 {
+			blocks[iteration] += 1
+			max -= 1
+			iteration += 1
+			if iteration == blocks.count { iteration = 0 }
+		}
+		counter += 1
+		if states.contains(blocks) { break }
+		states.append(blocks)
 	}
-	blocks[index] = 0
-	var iteration = index < blocks.count - 1 ? index + 1 : 0
-	while max > 0 {
-		blocks[iteration] += 1
-		max -= 1
-		iteration += 1
-		if iteration == blocks.count { iteration = 0 }
-	}
-	counter += 1
-	if states.contains(blocks) { break }
-	states.append(blocks)
+	return counter
 }
 
-print(counter)
+func partTwo() -> Int {
+	var blocks = input.split(separator: "\t").map { Int($0)! }
+	let initialState = blocks
+	var states = [initialState]
+	
+	var counter = 0
+	while true {
+		var max = Int.min
+		var index = 0
+		for i in 0..<blocks.count {
+			if blocks[i] > max {
+				max = blocks[i]
+				index = i
+			}
+		}
+		blocks[index] = 0
+		var iteration = index < blocks.count - 1 ? index + 1 : 0
+		while max > 0 {
+			blocks[iteration] += 1
+			max -= 1
+			iteration += 1
+			if iteration == blocks.count { iteration = 0 }
+		}
+		counter += 1
+		if states.contains(blocks) { break }
+		states.append(blocks)
+	}
+	return counter - states.index(of: blocks)!
+}
+
+print(partOne())
+print(partTwo())
